a106e1aa77899af2a19dbc46f744752747e52a7e
duguohui <1020261033@qq.com>
2020-10-23 11:30:26 +0800

    111

---
+++ cloud-consumers/src/main/java/com/cn/test/dgh/consumers/ConsumersMain.java
@@ -0,0 +1,12 @@
+package com.cn.test.dgh.consumers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumersMain {
+
+    public static void  main(String [] args){
+        SpringApplication.run(ConsumersMain.class);
+    }
+}
