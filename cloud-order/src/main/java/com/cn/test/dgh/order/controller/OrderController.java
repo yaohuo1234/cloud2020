fd7c03d120396edb98054da369f04350b47c7922
duguohui <1020261033@qq.com>
2020-10-23 11:29:25 +0800

    111

--- cloud-order/src/main/java/com/cn/test/dgh/order/controller/OrderController.java
+++ cloud-order/src/main/java/com/cn/test/dgh/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.cn.test.dgh.order.controller;
 
 import com.cn.test.dgh.jar.results.CommonResp;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/getById/{id}")
     public CommonResp<String> getOrder(@PathVariable("id") String id){
-        return CommonResp.buildSuccess(id);
+        return CommonResp.buildSuccess(appName+":"+id);
     }
 
 }
