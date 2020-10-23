aab3679b7d1d76ed6392e83a9c21303a8bac9d47
duguohui <1020261033@qq.com>
2020-10-23 10:49:43 +0800

    111

--- cloud-order/src/main/java/com/cn/test/dgh/order/OrderMain.java
+++ cloud-order/src/main/java/com/cn/test/dgh/order/OrderMain.java
@@ -2,7 +2,6 @@ package com.cn.test.dgh.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
