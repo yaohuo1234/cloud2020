8fb5d0b728c290703a5b939a62bd2b537bfeb020
duguohui <1020261033@qq.com>
2020-10-28 20:27:50 +0800

    hystrix-dashboard

--- cloud-consumers/src/main/java/com/cn/test/dgh/consumers/service/ConsumersService.java
+++ cloud-consumers/src/main/java/com/cn/test/dgh/consumers/service/ConsumersService.java
@@ -3,6 +3,7 @@ package com.cn.test.dgh.consumers.service;
 import com.cn.test.dgh.consumers.fegin.OrderFeigin;
 import com.cn.test.dgh.jar.results.CommonResp;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +16,21 @@ public class ConsumersService {
     @Resource
     private OrderFeigin  orderFeigin;
 
-    @HystrixCommand(fallbackMethod="getHystrixOrderById")
+    @HystrixCommand(fallbackMethod="getHystrixOrderById",commandProperties = {
+            @HystrixProperty(name="circuitBreaker.enabled",value = "true"),//是否开启断路器
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "10"),//请求次数
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//时间窗口
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "60")//失败率达到多少跳闸
+
+    })
     public CommonResp <String> getOrderById(String id){
-        int a=10/0;
+        if(Integer.valueOf(id)>0){
+            int a=10/0;
+        }
         return  orderFeigin.getOrder(id);
     }
 
     public CommonResp<String>  getHystrixOrderById(String id){
-        return CommonResp.buildError("系统繁忙,请稍后重试");
+        return CommonResp.buildError("系统繁忙,请稍后重试:"+id);
     }
 }
